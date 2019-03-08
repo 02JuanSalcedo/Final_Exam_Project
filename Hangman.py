8aad60fd62988e043a9257627a50b79776c0a9cb
Gordo <48022413+02JuanSalcedo@users.noreply.github.com>
2019-03-08 09:47:24 -0500

    Add Randomize list and breakdown letters

--- Hangman.py
+++ Hangman.py
@@ -8,3 +8,10 @@ import sys
 
 print(dir(random))
 print(dir(sys))
+
+# Randomize list and break down into letters
+words = ['bread', 'clock', 'mouse', 'banana', 'suitcase', 'cheese', 'mayonnaise']
+
+answer = list(random.choice(words))
+
+print(answer)
